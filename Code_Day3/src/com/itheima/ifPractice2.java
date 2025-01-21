8a4fd3c64ee8a1118a3a48f7405535ec8557eb55
Dong <2870292419@qq.com>
2025-01-21 10:06:35 +0800

    2025.1.20

--- Code_Day3/src/com/itheima/ifPractice2.java
+++ Code_Day3/src/com/itheima/ifPractice2.java
@@ -1,7 +1,20 @@
 package com.itheima;
 
+import java.util.Scanner;
+
 public class ifPractice2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入你的座位号");
+        int seatNumber = sc.nextInt();
+        if (seatNumber >= 0 && seatNumber <= 100) {
+            if (seatNumber % 2 == 0) {
+                System.out.println("坐在右边");
+            } else {
+                System.out.println("坐在左边");
+            }
 
+        }
+        System.out.println("不存在此座位号");
     }
 }
